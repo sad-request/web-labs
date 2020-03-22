83ad4c4db39c71cc84137ad22e75ab0a5b31d6fa
M <MPbugaga@yandex.ru>
2020-03-22 16:16:24 +0300

    добавлено задание 2 для лаб 2

---
+++ lab 2/print_args/print_args/print_args/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Number of arguments: " + args.Length);
+                Console.Write("Arguments: ");
+                for (int i = 0; i < args.Length; i++)
+                    Console.Write("'"+ args[i] + "', ");
+                Console.ReadKey(true);
+            }
+            else
+            {
+                Console.WriteLine("No parameters were specified!");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
