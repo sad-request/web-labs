0032fb50a3b39fa9f1a3ba6cb39583e05e03b181
M <MPbugaga@yandex.ru>
2020-03-22 16:53:11 +0300

    добавлено задание 3 в лаб 2

---
+++ lab 2/remove_dubl/remove_dubl/remove_dubl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1 || args.Length < 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!\nUsage remove_duplicates.exe <input string>");
+                Console.ReadKey(true);
+                Environment.Exit(1);
+            }
+            else
+            {
+            	args[0] = String.Concat(args[0].Distinct());
+            	Console.WriteLine(args[0]);
+            	Console.ReadKey(true);
+            }
+        }
+    }
+}
