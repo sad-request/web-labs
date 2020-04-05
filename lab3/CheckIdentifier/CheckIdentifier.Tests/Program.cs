8a0e878ee2f6594b5da14e25c6bf124067768e6f
M <MPbugaga@yandex.ru>
2020-04-05 19:53:35 +0300

    Добавлено задание 1 в лаб №3

---
+++ lab3/CheckIdentifier/CheckIdentifier.Tests/Program.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckIdentifierTests
+{
+    
+    [TestClass]
+    public class IsLatinLetter
+    {
+        [TestMethod]
+        public void DigitIsNotLatin()
+        {
+            char digit = '0';
+            bool result = CheckIdentifier.Program.IsLatinLetter(digit);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void RussianIsNotLatin()
+        {
+            char letter = 'м';
+            bool result = CheckIdentifier.Program.IsLatinLetter(letter);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod]
+        public void SpecialSymbolIsNotLatin()
+        {
+            char letter = '#';
+            bool result = CheckIdentifier.Program.IsLatinLetter(letter);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod]
+        public void SpaceIsNotLatin()
+        {
+            char letter = ' ';
+            bool result = CheckIdentifier.Program.IsLatinLetter(letter);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void LatinTrue()
+        {
+            char letter = 's';
+            bool result = CheckIdentifier.Program.IsLatinLetter(letter);
+            Assert.AreEqual(true, result);
+        }
+    }
+
+    [TestClass]
+    public class IsLatinLettersOrDigit
+    {
+        
+        [TestMethod]
+        public void SpaceIsNotLetterOrDigit()
+        {
+            string str = "hello world";
+            bool result = CheckIdentifier.Program.IsLatinLettersOrDigit(str);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod]
+        public void EmptyStringIsNotIncludeLetterOrDigit()
+        {
+            string str = "";
+            bool result = CheckIdentifier.Program.IsLatinLettersOrDigit(str);
+            Assert.AreEqual(true, result);
+
+        [TestMethod]
+        public void RussianIsNotLetterOrDigit()
+        {
+            string str = "helloмworld";
+            bool result = CheckIdentifier.Program.IsLatinLettersOrDigit(str);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod]
+        public void SpecialSymbolIsNotLetterOrDigit()
+        {
+            string str = "hello%world";
+            bool result = CheckIdentifier.Program.IsLatinLettersOrDigit(str);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod]
+        public void IsLetterOrDigit()
+        {
+            string str = "hello1";
+            bool result = CheckIdentifier.Program.IsLatinLettersOrDigit(str);
+            Assert.AreEqual(true, result);
+        }
+    }
+
+    [TestClass]
+    public class TakeArgsTests
+    {
+    	
+        [TestMethod]
+        public void TooManyArgs()
+        {
+            string[] str = { "black", "sabbath" };
+            string input = "";
+            bool result = CheckIdentifier.Program.ParseArgs(str, ref input);
+            Assert.AreEqual(false, result);
+        }
+    	
+        [TestMethod]
+        public void TooFewArgs()
+        {
+            string[] str = {};
+            string input = "";
+            bool result = CheckIdentifier.Program.TakeArgs(str, ref input);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod]
+        public void CorrectArgsNumber()
+        {
+            string[] str = { "black" };
+            string input = "";
+            bool result = CheckIdentifier.Program.ParseArgs(str, ref input);
+            Assert.AreEqual(true, result);
+        }
+    }
+
+    
+    [TestClass]
+    public class CheckIdentifierTests
+    {
+        [TestMethod]
+        public void StartWithDigitIsWrongIdentifier()
+        {
+            string str = "4identifier";
+            bool result = CheckIdentifier.Program.CheckIdentifier(str);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void SpecialSymbolIncludingIsWrongIdentifier()
+        {
+            string str = "wrong@identifier";
+            bool result = CheckIdentifier.Program.CheckIdentifier(str);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod]
+        public void RussianIncludeWrongIdentifier()
+        {
+            string str = "identifierрусский";
+            bool result = CheckIdentifier.Program.CheckIdentifier(str);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod]
+        public void EmptyStringIsWrongIdenrifier()
+        {
+            string str = "";
+            bool result = CheckIdentifier.Program.CheckIdentifier(str);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void CorrectIdentifier()
+        {
+            string str = "identifier9";
+            bool result = CheckIdentifier.Program.CheckIdentifier(str);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ lab3/CheckIdentifier/CheckIdentifier/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace CheckIdentifier
+{	    
+    public static class Program
+    {   
+
+		const string NoAnswer = "No";
+    	const string YesAnswer = "Yes";  
+    	
+    	public static bool IsLatinLetter(char ch)
+        {
+            if ((ch >= 'A') && (ch <= 'Z') || (ch >= 'a') && (ch <= 'z'))
+            {
+                return true;
+            }
+            return false;
+        }
+    	        
+		public static bool IsLatinLettersOrDigit(string input)
+        {
+	        foreach (char ch in input)
+	            {
+	                if (!IsLatinLetter(ch) && !char.IsDigit(ch))
+	                {
+	                    return false;
+	                }
+	            }
+	        	return true;
+		}
+        
+        public static bool CheckIdentifier(string input)
+        {
+            if (!IsLatinLettersOrDigit(input.Substring(1)))
+            {
+                System.Console.WriteLine(NoAnswer);
+                System.Console.WriteLine("Identifier must include only Latin letters or digits(not the first character)");
+                return false;
+            }
+            if (!IsLatinLetter(input[0]))
+            {
+                System.Console.WriteLine(NoAnswer);
+                System.Console.WriteLine("Identifier must begin with a letter");
+                return false;
+            }
+            if (input == "")
+            {
+                System.Console.WriteLine(NoAnswer);
+                System.Console.WriteLine("Empty identifier");
+                return false;
+            }
+            System.Console.WriteLine(YesAnswer);
+            return true;
+        }
+
+        public static bool TakeArgs(string[] args, ref string input)
+        {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine(NoAnswer);
+                System.Console.WriteLine("Incorrect number of arguments!\nUsage CheckIdentifier.exe <input string>" );
+                return false;
+            }
+            input = args[0];
+            return true;
+        }
+
+        
+        public static int Main(string[] args)
+        {
+            string input = "";
+            if (!TakeArgs(args, ref input))
+            {
+                return 1;
+            }
+            CheckIdentifier(input);
+            
+
+            return 0;
+        }
+    }
+}
