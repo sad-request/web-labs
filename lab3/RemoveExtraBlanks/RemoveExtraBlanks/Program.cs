c52b23406b8d54227cdc417f1936092635577e97
M <MPbugaga@yandex.ru>
2020-04-12 12:49:22 +0300

    добавлено задание 2 в лаб 3

---
+++ lab3/RemoveExtraBlanks/RemoveExtraBlanks/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RemoveExtraBlanks
+{
+    public class Program
+    {
+    	public static bool ParseArgs(string[] args, string inputName, string outputName)
+        {
+            if (args.Length != 2)
+            {
+                System.Console.WriteLine("Incorrect number of arguments!\nUsage RemoveExtraBlanks.exe <input file name> <output file name>");
+                return false;
+            }
+        inputName = args[0];
+        outputName = args[1];
+        return true;
+        }
+
+        public static bool ProcessData(string line, string inputFileName, string outputFileName)
+        {
+            if (!File.Exists(inputFileName))
+            {
+                Console.WriteLine("File doesn't exist");
+                return false;
+            }
+
+            StreamReader inputFile = new StreamReader(inputFileName);
+            StreamWriter outputFile = new StreamWriter(outputFileName);
+
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                outputFile.WriteLine(RemoveExtraBlanks(line));
+            }
+
+            inputFile.Close();
+            outputFile.Close();
+            return true;
+        }
+      
+        public static string RemoveExtraBlanks(string line)
+        {
+            line = line.Trim();
+            Regex regexText = new Regex(@"[ , \t]+");
+            return regexText.Replace(line, " ");
+        }
+        
+        static int Main(string[] args)
+        {
+            if (!ParseArgs(args, inputName, outputName))
+            {
+                return 1;
+            }
+
+            if (!ProcessData(line, inputName, outputName))
+            {
+                return 1;
+            }
+
+            return 0;
+        }       
+    }
+}
---
+++ lab3/RemoveExtraBlanks/RemoveExtraBlanksTests/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveExtraBlanks;
+
+namespace RemoveExtraBlanksTest
+{
+    [TestClass]
+    public class InputFilesTests
+    {
+    	[TestMethod]
+        public void CheckTooManyArguments()
+        {
+            string[] args = new string[] { "input.txt", "output.txt", "name.txt" };
+            bool res = Program.CheckArguments(args);
+            Assert.AreEqual(false, res);
+        }
+    	
+        [TestMethod]
+        public void CheckTooFewArguments()
+        {
+            string[] args = new string[] { "input.txt" };
+            bool res = Program.CheckArguments(args);
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void CorrectArgumentNumber()
+        {
+            string[] args = new string[] { "input.txt", "output.txt" };
+            bool res = Program.CheckArguments(args);
+            Assert.AreEqual(true, res);
+        }
+    }
+
+    [TestClass]
+    public class RemoveExtraBlanksTests
+    {
+        [TestMethod]
+        public void RemoveTabsAtEdges()
+        {
+            string line = "\hello\t";
+            line = Program.RemoveExtraBlanks(line);
+            Assert.AreEqual("hello", line);  
+        }
+
+        [TestMethod]
+        public void RemoveSpacesAtEdges()
+        {
+            string line = "  hello   ";
+            line = Program.RemoveExtraBlanks(line);
+            Assert.AreEqual("hello", line);
+        }
+        
+       public void RemoveSpacesAndTabsAtEdges()
+        {
+            string line = "     hello\t";
+            line = Program.RemoveExtraBlanks(line);
+            Assert.AreEqual("hello", line);  
+        }
+
+        [TestMethod]
+        public void RemoveSpacesBetweenWords()
+        {
+            string line = "hello      world";
+            line = Program.RemoveExtraBlanks(line);
+            Assert.AreEqual("hello world", line);
+        }
+        
+        [TestMethod]
+        public void RemoveTabsBetweenWords()
+        {
+            string line = "hello\t\t\tworld";
+            line = Program.RemoveExtraBlanks(line);
+            Assert.AreEqual("hello\tworld", line);
+        }
+
+        [TestMethod]
+        public void RemoveSpacesAndTabs()
+        {
+            string line = "  hello\t     my\t\t world  ";
+            line = Program.RemoveExtraBlanks(line);
+            Assert.AreEqual("hello my world", line);
+        }
+
+    }
+}
