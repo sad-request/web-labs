f32f1e9354e9792ea6d7ec80c8258af34f533172
M <MPbugaga@yandex.ru>
2020-04-14 19:41:06 +0300

    добавлено задание 3 в лаб №3

---
+++ lab3/PasswordStrength/PasswordStrength/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+
+
+namespace PasswordStrength
+{
+    public class Program
+    {   	
+        public static int CalculateSecurityByLenght(string password)
+        {
+            return 4 * password.Length;
+        }
+        
+        public static int CalculateSecurityByNumbersTotal(string password)
+        {
+            int count = password.Count(char.IsDigit).Count();
+            return count *= 4;
+        }
+
+         public static int CalculateSecurityByUpperCaseLettersTotal(string password)
+        {
+         	var count = 0;
+            int upperLetterCount = password.Count(char.IsUpper);
+            if (upperLetterCount != 0)
+            {
+                return (count - upperLetterCount) * 2;
+            }
+            return 0;
+        }
+
+        public static int CalculateSecurityByLowerCaseLettersTotal(string password)
+        {
+        	var count = 0;
+            int lowerLetterCount = password.Count(char.IsLower);
+            if (lowerLetterCount != 0)
+            {
+                return (count - lowerLetterCount) * 2;
+            }
+            return 0;
+        }
+        
+        public static int CalculateSecurityByOnlyLetters(string password)
+        {
+            var count = password.Count(char.IsLetter);
+            if (count == password.Length)
+            {
+                return -count;
+            }
+            return 0;
+        }
+        
+        public static int CalculateSecurityByOnlyDigits(string password)
+        {
+        	var count = 0;
+            int lowerLetterCount = password.Count(char.IsDigit);
+            if (lowerLetterCount == password.Length)
+            {
+                return -count;
+            }
+            return 0;
+        }
+        
+        public static int CalculateSecurityByReapetedSymbols(string password)
+        {
+            var result = 0;
+            var count = 0;
+            char prevCh = '\0';
+            foreach (var letter in password.Distinct().ToArray())
+            {
+                count = password.Count(chr => chr == letter);
+                if (count != 1)
+                {
+                    result += count;
+                }
+            }
+            return -result;
+        }
+        
+        public static int PasswordStrength(string password)
+        {
+            int passwordStrenght = 0;
+            passwordStrenght += CalculateSecurityByLenght(password);
+            passwordStrenght += CalculateSecurityByNumbersTotal(password);
+            passwordStrenght += CalculateSecurityByUpperCaseLettersTotal(password);
+            passwordStrenght += CalculateSecurityByLowerCaseLettersTotal(password);
+            passwordStrenght += CalculateSecurityByOnlyLetters(password);
+            passwordStrenght += CalculateSecurityByOnlyDigits(password);
+            passwordStrenght += CalculateSecurityByReapetedSymbols(password);
+            return passwordStrenght;
+        }
+    
+        public static bool ParseArg(string args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments\nUsage: PasswordStrength.exe <input password>");
+                return false;
+            }
+            return true;
+        }
+        
+        static int Main(string args)
+        {
+            if (!ParseArg(args))
+            {
+                return 1;
+            }
+            int passwordStrenght = PasswordStrength(args);
+            Console.WriteLine(passwordStrenght);
+            return 0;
+        }
+    }
+}
---
+++ lab3/PasswordStrength/PasswordStrengthTests/Program.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordStrength;
+
+namespace PasswordStrengthTests
+{
+    [TestClass]
+    public class CalculateSecurityByLenghtTest
+    {
+        [TestMethod]
+        public void PasswordStrengthIs4()
+        {
+            string password = "a";
+            int requiredStrength = 4;
+            int res = Program.CalculateSecurityByLenght(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void PasswordStrengthIs20()
+        {
+            string password = "cat12";
+            int requiredStrength = 20;
+            int res = Program.CalculateSecurityByLenght(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+    }
+
+    [TestClass]
+    public class CalculateSecurityByOnlyDigitsTest
+    {
+        [TestMethod]
+        public void OnlyDigitTest()
+        {
+            string password = "12345";
+            int requiredStrength = 20;
+            int res = Program.CalculateSecurityByOnlyDigits(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+
+        [TestMethod]
+        public void DigitsAndLettersMustIgnorLetters()
+        {
+            string password = "letter12";
+            int requiredStrength = 8;ы
+            int res = Program.CalculateSecurityByOnlyDigits(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+    }
+
+    [TestClass]
+    public class CalculateSecurityByUpperCaseLettersTotalTest
+    {
+        [TestMethod]
+        public void OnlyUpperCaseIs0()
+        {
+            string password = "ONLYUPPER";
+            int requiredStrength = 0;
+            int res = Program.CalculateSecurityByUpperCaseLettersTotal(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void UpperAndLower10()
+        {
+            string password = "UPPERlower";
+            int requiredStrength = 10;
+            int res = Program.CalculateSecurityByUpperCaseLettersTotal(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void OnlyLowerIs0()
+        {
+            string password = "hello123";
+            int requiredStrength = 0;
+            int res = Program.CalculateSecurityByUpperCaseLettersTotal(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+    }
+
+    [TestClass]
+    public class CalculateSecurityByLowerCaseLettersTotalTest
+    {
+        [TestMethod]
+        public void OnlyUpperCaseIs0()
+        {
+            string password = "ONLYUPPER";
+            int requiredStrength = 0;
+            int res = Program.CalculateSecurityByLowerCaseLettersTotal(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void UpperAndLower10()
+        {
+            string password = "UPPERlower";
+            int requiredStrength = 10;
+            int res = Program.CalculateSecurityByLowerCaseLettersTotal(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void OnlyLowerIs0()
+        {
+            string password = "onlylower";
+            int requiredStrength = 0;
+            int res = Program.CalculateSecurityByLowerCaseLettersTotal(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+    }
+
+    [TestClass]
+    public class CalculateSecurityByOnlyLettersTest
+    {
+        [TestMethod]
+        public void password_absc123_should_return_strength_equals_to_0()
+        {
+            string password = "absc123";
+            int requiredStrength = 0;
+            int res = Program.CalculateSecurityByOnlyLetters(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void OnlyLetters()
+        {
+            string password = "AAsBBsXaaa";
+            int requiredStrength = -10;
+            int res = Program.CalculateSecurityByOnlyLetters(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        }
+    }
+
+    [TestClass]
+    public class CalculateSecurityByOnlyDigitsTest
+    {
+        [TestMethod]
+        public void DigitsAndLettesTest()
+        {
+            string password = "word123";
+            int requiredStrength = 0;
+            int res = Program.CalculateSecurityByOnlyDigits(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void OnlyDigitsTest()
+        {
+            string password = "123456";
+            int requiredStrength = -6;
+            int res = Program.CalculateSecurityByOnlyDigits(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void OnlyLettersTest()
+        {
+            string password = "hello";
+            int requiredStrength = 0;
+            int res = Program.CalculateSecurityByOnlyDigits(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+    }
+
+    [TestClass]
+    public class CalculateSecurityByReapetedSymbolsTest
+    {
+        [TestMethod]
+        public void OnlySameSymbols()
+        {
+            string password = "aaa";
+            int requiredStrength = -3;
+            int res = Program.CalculateSecurityByReapetedSymbols(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void NotOnlySameSymbols()
+        {
+            string password = "helloworld";
+            int requiredStrength = -4;
+            int res = Program.CalculateSecurityByReapetedSymbols(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+        [TestMethod]
+        public void NoSameSymbols()
+        {
+            string password = "word";
+            int requiredStrength = -10;
+            int res = Program.CalculateSecurityByReapetedSymbols(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+        
+    }
+
+    [TestClass]
+    public class PasswordStrengthTest
+    {    	
+        [TestMethod]
+        public void SameSymbols()
+        {
+            string password = "aaaa";
+            int requiredStrength = 8;
+            int res = Program.PasswordStrength(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+        
+        [TestMethod]
+        public void OnlyDigits()
+        {
+            string password = "123456";
+            int requiredStrength = 42;
+            int res = Program.PasswordStrength(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+        
+        [TestMethod]
+        public void NormalPassword()
+        {
+            string password = "world123";
+            int requiredStrength = 54;
+            int res = Program.PasswordStrength(password);
+            Assert.AreEqual(requiredStrength, res);
+        }
+
+    }
+}
